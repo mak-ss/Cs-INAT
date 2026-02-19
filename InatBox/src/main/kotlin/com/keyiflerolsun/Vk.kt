2b4686f9aebf677b622b3fb84d3351ae299d534b
mak-ss <mehmet.kocatepe@gmail.com>
2026-02-20 00:16:34 +0300

    Create Vk.kt

---
+++ InatBox/src/main/kotlin/com/keyiflerolsun/Vk.kt
@@ -0,0 +1,44 @@
+package com.keyiflerolsun
+
+import com.lagradost.cloudstream3.*
+import com.lagradost.cloudstream3.utils.*
+
+class Vk : ExtractorApi() {
+
+    override val name = "Vk"
+    override val mainUrl = "https://vk.com/"
+    override val requiresReferer = false
+
+    override suspend fun getUrl(
+        url: String,
+        referer: String?,
+        subtitleCallback: (SubtitleFile) -> Unit,
+        callback: (ExtractorLink) -> Unit
+    ) {
+
+        val response = app.get(
+            url,
+            headers = mapOf("X-Requested-With" to "XMLHttpRequest"),
+            referer = mainUrl
+        )
+
+        val m3u8Regex = Regex("\"([^\"]*m3u8[^\"]*)\"")
+        val m3u8SourceUrl =
+            m3u8Regex.find(response.text)
+                ?.groupValues?.get(1)
+                ?.replace("\\/", "/")
+
+        if (m3u8SourceUrl != null) {
+            callback.invoke(
+                ExtractorLink(
+                    source = name,
+                    name = name,
+                    url = m3u8SourceUrl,
+                    referer = mainUrl,
+                    quality = Qualities.Unknown.value,
+                    type = ExtractorLinkType.M3U8
+                )
+            )
+        }
+    }
+}
