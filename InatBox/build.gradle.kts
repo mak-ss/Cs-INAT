f2b53eeb1d668a65661330b006cb043b52ec08fa
mak-ss <mehmet.kocatepe@gmail.com>
2026-02-20 00:56:00 +0300

    Delete InatBox directory

--- InatBox/build.gradle.kts
+++
@@ -1,9 +0,0 @@
-version = 17
-
-cloudstream {
-    authors     = listOf("JustRelaxable", "keyiflerolsun")
-    language    = "tr"
-    description = "İnatBox cloudstream eklentisidir. Güncel API ve kararlı sürüm desteği."
-    status      = 1 // 1: Ok (Çalışıyor)
-    tvTypes     = listOf("Movie", "TvSeries", "Live")
-}
