6303b930ad26ab79fc81a065951f482993a0375a
mak-ss <mehmet.kocatepe@gmail.com>
2026-02-20 00:15:37 +0300

    Update DiskYandexComTrExtractor.kt

--- InatBox/src/main/kotlin/com/keyiflerolsun/DiskYandexComTrExtractor.kt
+++ InatBox/src/main/kotlin/com/keyiflerolsun/DiskYandexComTrExtractor.kt
@@ -5,24 +5,39 @@ import com.lagradost.cloudstream3.utils.*
 import java.util.regex.Pattern
 
 class DiskYandexComTr : ExtractorApi() {
-    override val name: String = "DiskYandexComTr"
-    override val mainUrl: String = "https://disk.yandex.com.tr"
-    override val requiresReferer: Boolean = false
-    private val masterPlaylistRegex = Pattern.compile("https?://[^\\s\"]*?master-playlist\\.m3u8")
 
-    override suspend fun getUrl(url: String, referer: String?, subtitleCallback: (SubtitleFile) -> Unit, callback: (ExtractorLink) -> Unit) {
-        val request = app.get(url, referer = "https://disk.yandex.com.tr/", headers = mapOf("X-Requested-With" to "XMLHttpRequest"))
-        if (!request.isSuccessful) return
+    override val name = "Yandex Disk"
+    override val mainUrl = "https://disk.yandex.com.tr"
+    override val requiresReferer = false
 
-        val matcher = masterPlaylistRegex.matcher(request.text)
+    private val masterPlaylistRegex =
+        Pattern.compile("https?://[^\\s\"]*?master-playlist\\.m3u8")
+
+    override suspend fun getUrl(
+        url: String,
+        referer: String?,
+        subtitleCallback: (SubtitleFile) -> Unit,
+        callback: (ExtractorLink) -> Unit
+    ) {
+
+        val response = app.get(
+            url,
+            referer = mainUrl,
+            headers = mapOf("X-Requested-With" to "XMLHttpRequest")
+        )
+
+        if (!response.isSuccessful) return
+
+        val matcher = masterPlaylistRegex.matcher(response.text)
+
         if (matcher.find()) {
             callback.invoke(
                 ExtractorLink(
-                    "Yandex Disk",
-                    "Yandex Disk",
-                    matcher.group(),
-                    referer ?: "",
-                    Qualities.Unknown.value,
+                    source = name,
+                    name = name,
+                    url = matcher.group(),
+                    referer = mainUrl,
+                    quality = Qualities.Unknown.value,
                     type = ExtractorLinkType.M3U8
                 )
             )
