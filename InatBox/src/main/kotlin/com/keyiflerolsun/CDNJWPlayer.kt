571835dc706add9fd1b85244e96e6b7fd581e95f
mak-ss <mehmet.kocatepe@gmail.com>
2026-02-20 00:03:22 +0300

    Update CDNJWPlayer.kt

--- InatBox/src/main/kotlin/com/keyiflerolsun/CDNJWPlayer.kt
+++ InatBox/src/main/kotlin/com/keyiflerolsun/CDNJWPlayer.kt
@@ -8,20 +8,15 @@ class CDNJWPlayer : ExtractorApi() {
     override val mainUrl: String = "https://cdn.jwplayer.com"
     override val requiresReferer: Boolean = false
 
-    override suspend fun getUrl(
-        url: String,
-        referer: String?,
-        subtitleCallback: (SubtitleFile) -> Unit,
-        callback: (ExtractorLink) -> Unit
-    ) {
+    override suspend fun getUrl(url: String, referer: String?, subtitleCallback: (SubtitleFile) -> Unit, callback: (ExtractorLink) -> Unit) {
         callback.invoke(
             ExtractorLink(
-                source  = this.name,
-                name    = this.name,
-                url     = url,
-                referer = referer ?: "",
-                quality = Qualities.Unknown.value,
-                type    = ExtractorLinkType.M3U8
+                this.name,
+                this.name,
+                url,
+                referer ?: "",
+                Qualities.Unknown.value,
+                type = ExtractorLinkType.M3U8
             )
         )
     }
