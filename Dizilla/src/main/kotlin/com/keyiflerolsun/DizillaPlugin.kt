1a7cd03606fe750ec7f51c25b637aab114a0c1da
mak-ss <mehmet.kocatepe@gmail.com>
2026-02-21 14:15:07 +0300

    Add files via upload

---
+++ Dizilla/src/main/kotlin/com/keyiflerolsun/DizillaPlugin.kt
@@ -0,0 +1,20 @@
+
+package com.keyiflerolsun
+
+import android.content.Context
+import com.lagradost.cloudstream3.plugins.CloudstreamPlugin
+import com.lagradost.cloudstream3.plugins.Plugin
+
+@CloudstreamPlugin
+class DizillaPlugin: Plugin() {
+    override fun load(context: Context) {
+        registerMainAPI(Dizilla())
+        registerExtractorAPI(ContentX())
+        registerExtractorAPI(Hotlinger())
+        registerExtractorAPI(FourCX())
+        registerExtractorAPI(PlayRu())
+        registerExtractorAPI(FourPlayRu())
+        registerExtractorAPI(FourPichive())
+        registerExtractorAPI(Pichive())
+    }
+}
