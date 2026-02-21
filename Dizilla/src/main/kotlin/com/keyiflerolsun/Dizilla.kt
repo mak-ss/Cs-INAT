e4172d1e2b93ad7bbb8193ee990d4239b388160e
mak-ss <mehmet.kocatepe@gmail.com>
2026-02-21 21:49:36 +0300

    Update Dizilla.kt

--- Dizilla/src/main/kotlin/com/keyiflerolsun/Dizilla.kt
+++ Dizilla/src/main/kotlin/com/keyiflerolsun/Dizilla.kt
@@ -1,7 +1,20 @@
 package com.keyiflerolsun
 
 import com.fasterxml.jackson.databind.ObjectMapper
-import com.lagradost.cloudstream3.*
+import com.lagradost.cloudstream3.MainAPI
+import com.lagradost.cloudstream3.HomePageResponse
+import com.lagradost.cloudstream3.MainPageRequest
+import com.lagradost.cloudstream3.SearchResponse
+import com.lagradost.cloudstream3.LoadResponse
+import com.lagradost.cloudstream3.TvType
+import com.lagradost.cloudstream3.newMovieSearchResponse
+import com.lagradost.cloudstream3.newTvSeriesSearchResponse
+import com.lagradost.cloudstream3.newMovieLoadResponse
+import com.lagradost.cloudstream3.newTvSeriesLoadResponse
+import com.lagradost.cloudstream3.newEpisode
+import com.lagradost.cloudstream3.Episode
+import com.lagradost.cloudstream3.SubtitleFile
+import com.lagradost.cloudstream3.app
 import com.lagradost.cloudstream3.utils.ExtractorLink
 import com.lagradost.cloudstream3.utils.loadExtractor
 import org.jsoup.Jsoup
