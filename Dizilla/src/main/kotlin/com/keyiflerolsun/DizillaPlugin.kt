739a10de0a90a94b9cc8e37a75e5cdd8652c8d72
mak-ss <mehmet.kocatepe@gmail.com>
2026-02-21 21:35:30 +0300

    Update DizillaPlugin.kt

--- Dizilla/src/main/kotlin/com/keyiflerolsun/DizillaPlugin.kt
+++ Dizilla/src/main/kotlin/com/keyiflerolsun/DizillaPlugin.kt
@@ -5,7 +5,7 @@ import com.lagradost.cloudstream3.plugins.Plugin
 import android.content.Context
 
 @CloudstreamPlugin
-class SelcukFlixPlugin: Plugin() {
+class DizillaPlugin: Plugin() {
     override fun load(context: Context) {
         registerMainAPI(SelcukFlix())
     }
