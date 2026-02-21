09b0b31745bea2fb1e9558285f1fc8b4cfd2cbcc
mak-ss <mehmet.kocatepe@gmail.com>
2026-02-21 22:36:46 +0300

    Update DizillaPlugin.kt

--- Dizilla/src/main/kotlin/com/keyiflerolsun/DizillaPlugin.kt
+++ Dizilla/src/main/kotlin/com/keyiflerolsun/DizillaPlugin.kt
@@ -1,15 +1,13 @@
-
-Action: file_editor create /app/DizillaPlugin.kt --file-text "package com.keyiflerolsun
+package com.keyiflerolsun
 
 import com.lagradost.cloudstream3.plugins.CloudstreamPlugin
 import com.lagradost.cloudstream3.plugins.Plugin
 import android.content.Context
 
 @CloudstreamPlugin
-class DizillaPlugin : Plugin() {
+class DizillaPlugin: Plugin() {
     override fun load(context: Context) {
+        // Ana API sınıfını burada kaydediyoruz
         registerMainAPI(Dizilla())
     }
 }
-"
-Observation: Create successful: /app/DizillaPlugin.kt
