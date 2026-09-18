f57dbc7b15cf201deedb89f484c491b7c55bbb26
mak-ss <mehmet.kocatepe@gmail.com>
2026-09-18 15:37:02 +0300

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,18 +10,17 @@ buildscript {
     }
     dependencies {
         classpath("com.android.tools.build:gradle:8.13.2")
-        // commit hash'inin başına 'v' ekleyerek JitPack versiyon çakışmasını engelliyoruz
-        classpath("com.github.recloudstream:gradle:vcce1b8d84d") 
+        // JitPack sürüm çakışmasını önlemek için tam commit hash'i tanımlandı
+        classpath("com.github.recloudstream:gradle:cce1b8d84d5df65d8366e6b4329e4bd43eb4eb92")
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:2.3.0")
     }
 }
-
 
 allprojects {
     repositories {
         google()
         mavenCentral()
-        maven("https://jitpack.io")
+        maven { url = uri("https://jitpack.io") }
     }
 }
 
@@ -30,8 +29,6 @@ fun Project.cloudstream(configuration: CloudstreamExtension.() -> Unit) = extens
 fun Project.android(configuration: BaseExtension.() -> Unit) {
     extensions.getByName<BaseExtension>("android").apply {
         (extensions.findByName("java") as? JavaPluginExtension)?.apply {
-            // Use Java 17 toolchain even if a higher JDK runs the build.
-            // We still use Java 8 for now which higher JDKs have deprecated.
             toolchain {
                 languageVersion.set(JavaLanguageVersion.of(17))
             }
@@ -47,9 +44,7 @@ subprojects {
     apply(plugin = "com.lagradost.cloudstream3.gradle")
 
     cloudstream {
-        // when running through github workflow, GITHUB_REPOSITORY should contain current repository name
         setRepo(System.getenv("GITHUB_REPOSITORY") ?: "https://github.com/Kraptor123/Cs-Karma")
-
         authors = listOf("kraptor")
     }
 
@@ -86,18 +81,14 @@ subprojects {
         val cloudstream by configurations
         val implementation by configurations
 
-        // Stubs for all Cloudstream classes
         cloudstream("com.lagradost:cloudstream3:pre-release")
 
-        // these dependencies can include any of those which are added by the app,
-        // but you dont need to include any of them if you dont need them
-        // https://github.com/recloudstream/cloudstream/blob/master/app/build.gradle
-        implementation(kotlin("stdlib"))                                             // Kotlin'in temel kütüphanesi
-        implementation("com.github.Blatzar:NiceHttp:0.4.13")                          // HTTP kütüphanesi
-        implementation("org.jsoup:jsoup:1.22.1")                                      // HTML ayrıştırıcı
-        implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.13.5")   // Kotlin için Jackson JSON kütüphanesi
-        implementation("com.fasterxml.jackson.core:jackson-databind:2.13.5")          // JSON-nesne dönüştürme kütüphanesi
-        implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.10.2")      // Kotlin için asenkron işlemler
+        implementation(kotlin("stdlib"))
+        implementation("com.github.Blatzar:NiceHttp:0.4.13")
+        implementation("org.jsoup:jsoup:1.22.1")
+        implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.13.5")
+        implementation("com.fasterxml.jackson.core:jackson-databind:2.13.5")
+        implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.10.2")
         implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.9.0")
         implementation("androidx.appcompat:appcompat:1.7.1")
     }
